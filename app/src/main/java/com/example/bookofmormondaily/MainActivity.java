da3de67680c8eea36283c03aa983944716cbbb0d
Mitchell <msaat429@gmail.com>
2022-07-15 02:20:04 -0600

    Initial commit

---
+++ app/src/main/java/com/example/bookofmormondaily/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.bookofmormondaily;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.google.android.material.theme.MaterialComponentsViewInflater;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView day;
+    private TextView verse;
+    private TextView text;
+    private Button button;
+    LocalDate date;
+
+    DatePickerDialog DateDialog;
+
+    public static final String[] MONTHS = {"January", "February", "March",
+            "April", "May", "June", "July", "August", "September", "October",
+            "November", "December"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        day = findViewById(R.id.day);
+        date = LocalDate.now();
+        day.setText(date.getMonth().toString() + " " + date.getDayOfMonth());
+
+        Verses verses = new Verses();
+        int month = date.getMonthValue();
+        String[] scripture;
+        verse = findViewById(R.id.verse);
+        text = findViewById(R.id.text);
+
+        scripture = verses.getScripture(month, date.getDayOfMonth());
+        verse.setText(scripture[0]);
+        text.setText(scripture[1]);
+
+        LocalDate ld = LocalDate.now();
+        button = findViewById(R.id.change_day);
+        button.setOnClickListener(view -> {
+            DateDialog = new DatePickerDialog(MainActivity.this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                            day.setText(MONTHS[month] + " " + dayOfMonth);
+                            String[] scripture;
+                            scripture = verses.getScripture(month,dayOfMonth);
+                            verse.setText(scripture[0]);
+                            text.setText(scripture[1]);
+                        }
+                    }, 2022, ld.getMonthValue() - 1, ld.getDayOfMonth());
+            DateDialog.show();
+        });
+    }
+}
---
+++ app/src/main/java/com/example/bookofmormondaily/Verses.java
@@ -0,0 +1,143 @@
+package com.example.bookofmormondaily;
+
+public class Verses {
+
+    public String[] getScripture(int month, int day){
+        String[] scripture = new String[2];
+        scripture[0] = "1 Nephi 20:10";
+        scripture[1] = "For, behold, I have refined thee, I have chosen thee in the furnace of affliction.";
+
+        switch(month){
+            case 0:
+                switch(day){
+                    case 1:
+                        scripture[0] = "3 Nephi 13:33";
+                        scripture[1] = "But seek ye first the kingdom of God and his righteousness, and all these things shall be added unto you.";
+                        break;
+                    case 2:
+                        scripture[0] = "Mosiah 4:27";
+                        scripture[1] = "And see that all these things are done in wisdom and order; for it is not requisite that a man should run faster than he has strength. And again, it is expedient that he should be diligent, that thereby he might win the prize; therefore, all things must be done in order.";
+                        break;
+                    case 3:
+                        scripture[0] = "2 Nephi 2:25";
+                        scripture[1] = "Adam fell that men might be; and men care, that they might have joy.";
+                        break;
+                    case 4:
+                        scripture[0] = "Mosiah 9:6";
+                        scripture[1] = "And now, my beloved son, notwithstanding their hardness, let us labor diligently; for if we should cease to labor, we should be brought under condemnation; for we have a labor to perform whilst in this tabernacle of clay, that we may conquer the enemy of all righteousness, and rest our souls in the kingdom of God.";
+                        break;
+                    case 5:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 6:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 7:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 8:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 9:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 10:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 11:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 12:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 13:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 14:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 15:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 16:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 17:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 18:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 19:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 20:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 21:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 22:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 23:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 24:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 25:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 26:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 27:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 28:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 29:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 30:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                    case 31:
+                        scripture[0] = "";
+                        scripture[1] = "";
+                        break;
+                }
+                break;
+        }
+
+        return scripture;
+    }
+}
