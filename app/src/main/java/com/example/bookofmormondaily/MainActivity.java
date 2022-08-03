3b8b2c6fd4adb1f0f7f8d2c9d597e063958bcef6
Mitchell <msaat429@gmail.com>
2022-08-03 04:20:08 -0600

    add firebase

--- app/src/main/java/com/example/bookofmormondaily/MainActivity.java
+++ app/src/main/java/com/example/bookofmormondaily/MainActivity.java
@@ -1,24 +1,37 @@
 package com.example.bookofmormondaily;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.PopupWindow;
 import android.widget.TextView;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
+import android.widget.Toast;
 
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
 
     DatePickerDialog DateDialog;
 
+    private FirebaseAuth auth;
+
+    DatabaseReference databaseReference;
+
+
+
     public static final String[] MONTHS = {"January", "February", "March",
             "April", "May", "June", "July", "August", "September", "October",
             "November", "December"};
@@ -44,20 +63,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.topAppBar);
-//        setSupportActionBar(toolbar);
-//        toolbar.setNavigationIcon(R.drawable.ic_menu);
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                if (drawer.isDrawerOpen(Gravity.LEFT)) {
-//                    drawer.closeDrawer(Gravity.LEFT);
-//                } else {
-//                    drawer.openDrawer(Gravity.LEFT);
-//                }
-//            }
-//        });
+
+        auth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance("https://bomdaily-b0aea-default-rtdb.firebaseio.com/").getReference();
 
 
         day = findViewById(R.id.day);
@@ -103,5 +111,50 @@ public class MainActivity extends AppCompatActivity {
         imageButton = findViewById(R.id.feedbackButton);
 
 
+        imageButton.setOnClickListener(v ->{
+            View popupView = getLayoutInflater().inflate(R.layout.feedback, null);
+
+            EditText feedback = popupView.findViewById(R.id.feedback_text);
+            Button submit;
+            submit = popupView.findViewById(R.id.submit);
+
+            Button cancel;
+            cancel = popupView.findViewById(R.id.cancel);
+
+            PopupWindow popupWindow = new PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
+
+            popupWindow.setFocusable(true);
+            popupWindow.setBackgroundDrawable(new ColorDrawable());
+            popupWindow.showAtLocation(popupView, Gravity.CENTER, 0,-20);
+
+            submit.setOnClickListener(w ->{
+                LocalDateTime localDateTime = LocalDateTime.now();
+                String feedbackString = feedback.getText().toString();
+                localDateTime = localDateTime.truncatedTo(ChronoUnit.SECONDS);
+                String localdate = localDateTime.toString();
+                if(TextUtils.isEmpty(feedbackString)) {
+                    Toast.makeText(this, "Please fill out the text box.", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    databaseReference.child("Feedback").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                databaseReference.child("Feedback").child(localdate).setValue(feedbackString);
+                                Toast.makeText(MainActivity.this, "Thank you for your feedback!", Toast.LENGTH_SHORT).show();
+                                popupWindow.dismiss();
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Toast.makeText(MainActivity.this, "Failed to send feedback " + error, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+
+
+            });
+            cancel.setOnClickListener(w ->{
+                popupWindow.dismiss();
+            });
+        });
     }
 }
